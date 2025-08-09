14fba67d1bc00d83a5352f95e753f946e673b7a8
TsVch <vyacheslav.czoj@bk.ru>
2025-08-09 13:33:29 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, BigTech!");
+        System.out.println("Today is: "+LocalDate.now());
+        System.out.println("My goal: Become a BigTech Java Developer!");
     }
 }
